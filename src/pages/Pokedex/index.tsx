6022b6421f4c39ebd3924e984dd0ab9b7eed6736
Aseeva Julia <j.s.aseeva@mail.ru>
2020-11-13 20:30:07 +0300

    added hook

--- src/pages/Pokedex/index.tsx
+++ src/pages/Pokedex/index.tsx
@@ -1,35 +1,24 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import cn from 'classnames';
 
 import as from '../../App.module.scss';
 import ps from './Pokedex.module.scss';
-import Header from '../../components/Header';
+
 import Footer from '../../components/Footer';
 import PokemonCard from '../../components/PokemonCard';
 import Heading from '../../components/Heading';
 
-const PokedexPage: React.FC = () => {
-  const [totalPokemons, setTotalPokemons] = useState(0);
-  const [pokemons, setPokemons] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
+import usePokemons, { IData } from '../../hooks/usePokemons';
+import Layout from '../../components/Layout';
 
-  useEffect(() => {
-    setIsLoading(true);
-    fetch('http://zar.hosthot.ru/api/v1/pokemons?limit=9')
-      .then((res) => res.json())
-      .then((data) => {
-        setTotalPokemons(data.total);
-        setPokemons(data.pokemons);
-        setIsError(false);
-      })
-      .catch(() => {
-        setIsError(true);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-  }, []);
+interface PokedexPageProps {
+  data?: IData | undefined;
+  isLoading?: boolean;
+  isError?: boolean;
+}
+
+const PokedexPage: React.FC<PokedexPageProps> = () => {
+  const { data, isLoading, isError } = usePokemons();
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -41,23 +30,24 @@ const PokedexPage: React.FC = () => {
 
   return (
     <>
-      <Header />
-      <div className={ps.contentWrapper}>
+      <Layout className={ps.contentWrapper}>
         <div className={cn(as.container, ps.contentPokedex)}>
           <Heading tag="h2" propsClassName={ps.contentTitle}>
-            {totalPokemons} <strong>Pokemons</strong> for you to choose your favorite
+            {data?.total} <strong>Pokemons</strong> for you to choose your favorite
           </Heading>
           <div className={ps.cardsWrapper}>
-            {pokemons.map((item, index) => {
-              return (
-                <div key={`pokemon${index + 1}`}>
-                  <PokemonCard pokemon={item} />
-                </div>
-              );
-            })}
+            {data?.pokemons
+              ? data?.pokemons.map((item, index) => {
+                  return (
+                    <div key={`pokemon${index + 1}`}>
+                      <PokemonCard pokemon={item} />
+                    </div>
+                  );
+                })
+              : null}
           </div>
         </div>
-      </div>
+      </Layout>
       <Footer />
     </>
   );
