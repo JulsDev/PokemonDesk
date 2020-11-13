83bcff7b23b52fd3ebccb9740a719be9ea136945
Aseeva Julia <j.s.aseeva@mail.ru>
2020-11-13 20:43:21 +0300

    fix main content

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -17,20 +17,22 @@ import { LinkEnum } from '../../routes';
 const HomePage: React.FC = () => {
   return (
     <div className={hs.content__wrapper}>
-      <Layout className={cn(as.container, hs.content__home)}>
-        <div className={hs.content__text}>
-          <Heading tag="h1" propsClassName={hs.content__title}>
-            <strong>Find</strong> all your favorite <strong>Pokemon</strong>
-          </Heading>
-          <Heading tag="h2" propsClassName={hs.content__subtitle}>
-            You can know the type of Pokemon, its strengths, disadvantages and abilities
-          </Heading>
-          <Button onClick={() => navigate(LinkEnum.POKEDEX)} type="button" color="green" size={ButtonSize.medium}>
-            See pokemons
-          </Button>
-        </div>
-        <div className={hs.content__parallax}>
-          <Parallax />
+      <Layout className={cn(as.container)}>
+        <div className={hs.content__home}>
+          <div className={hs.content__text}>
+            <Heading tag="h1" propsClassName={hs.content__title}>
+              <strong>Find</strong> all your favorite <strong>Pokemon</strong>
+            </Heading>
+            <Heading tag="h2" propsClassName={hs.content__subtitle}>
+              You can know the type of Pokemon, its strengths, disadvantages and abilities
+            </Heading>
+            <Button onClick={() => navigate(LinkEnum.POKEDEX)} type="button" color="green" size={ButtonSize.medium}>
+              See pokemons
+            </Button>
+          </div>
+          <div className={hs.content__parallax}>
+            <Parallax />
+          </div>
         </div>
       </Layout>
       <Footer />
